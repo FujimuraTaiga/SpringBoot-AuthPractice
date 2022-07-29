d2514a0d3b83e1e6ac147acee6e9ef1720c5858f
taiga-mac <fujimurataiga@icloud.com>
2022-07-30 00:45:38 +0900

    initial commit

---
+++ SpringSecurity/src/main/java/com/example/javaauthorizationpractice/JavaAuthorizationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaauthorizationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuthorizationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAuthorizationPracticeApplication.class, args);
+    }
+
+}
---
+++ SpringSecurity/src/test/java/com/example/javaauthorizationpractice/JavaAuthorizationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaauthorizationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthorizationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
